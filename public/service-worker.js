032ae755c30e63c0dff080234910381ea0d04793
skphostel21 <skphostel21@gmail.com>
2026-09-05 14:56:10 +0530

    Update project features

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'kalam-block-shell-v4';
-const SHELL = ['/', '/index.html', '/manifest.json', '/icon.svg', '/assets/logo.webp', '/assets/icon-192.png', '/assets/icon-512.png'];
+const CACHE_NAME = 'kalam-block-shell-v5';
+const SHELL = ['/', '/index.html', '/manifest.json', '/icon.svg', '/assets/logo.webp', '/assets/icon-192.png', '/assets/icon-512.png', '/assets/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
