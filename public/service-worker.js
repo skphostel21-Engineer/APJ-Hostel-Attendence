e62d4be9bab24e20b16a39d29c786a57b766c8c4
skphostel21 <skphostel21@gmail.com>
2026-09-08 00:24:20 +0530

    Improve student portal and production features

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kalam-block-shell-v8';
+const CACHE_NAME = 'kalam-block-shell-v9';
 const SHELL = ['/', '/index.html', '/manifest.json', '/icon.svg', '/assets/logo.webp', '/assets/icon-192.png', '/assets/icon-512.png', '/assets/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
